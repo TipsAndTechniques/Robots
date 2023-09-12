e242413014e64afc3da2e10eedaec3a679487c81
Tips&Techniques <claude.dube@bell.net>
2023-09-12 09:49:47 -0400

    Créé Lisdte de robots initiale avec data

--- RobotWantedLeague/Controllers/RobotsController.cs
+++ RobotWantedLeague/Controllers/RobotsController.cs
@@ -4,12 +4,34 @@ using RobotWantedLeague.Models;
 
 namespace RobotWantedLeague.Controllers;
 
-public class Robots{
-    public int Id { get; set; }
-    public string? Name { get; set; } 
-    public string? Height { get; set; }
-    public string? Weight { get; set; }
-    public string? Pays { get; set; }
+public class Robot
+{
+
+    public int id { get; set; }
+    public string name { get; set; }
+    public string height_m { get; set; }
+    public string weight_kg { get; set; }
+    public string country { get; set; }
+
+    public Robot(int id, string name, string height_m, string weight_kg, string country)
+    {
+        this.id = id;
+        this.name = name;
+        this.height_m = height_m;
+        this.weight_kg = weight_kg;
+        this.country = country;
+    }
+
+}
+public static class RobotsService
+{
+    public static List<Robot> listeRobots = new List<Robot>()
+    {
+        new Robot(1, "Alice", "17.76 m", "6,578 kg", "Albanie"),
+        new Robot(2, "Bob", "15 m", "10,871 kg", "Bénin"),
+        new Robot(3, "Cédric", "3.12 m", "1,002 kg", "Cambodge")
+    };
+
 }
 
 public class RobotsController : Controller
